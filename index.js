48df59b67389f10a7e79232e494841de166eb5c9
Borhan Uddin <borhaninfos@gmail.com>
2022-02-15 22:17:08 +0600

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const redux = require("redux");
 // console.log(redux);
 
+// Reducer
+const rootReducer = (state, action) => {
+  return state;
+};
+
 // Create Store
 const store = redux.createStore();
 console.log(store);
